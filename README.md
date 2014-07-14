8192997b9db5e85fe9829fcf03290c049ea92be6
Alexey Lavrenuke (load testing) <direvius@yandex-team.ru>
2014-07-14 15:03:24 +0400

    Cosmetics

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-A Python library for querying metrics from Graphite into Pandas DataFrames
+A Python library for querying metrics from Graphite using its URL API into Pandas DataFrames. Use like this:
+
+```
+df = lg.Query(graphite) \
+        .target('aliasByMetric(five_sec.my.favourite.metric)') \
+        .pfrom('-3d') \
+        .puntil('-1d') \
+        .execute()
+```
--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ version = '0.1.3'
 
 setup(name='libgraphite',
       version=version,
-      description="Graphite client",
+      description="Graphite querying library",
       long_description="""\
+A Python library for querying metrics from Graphite into Pandas DataFrames
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Topic :: Software Development :: Libraries', 'Topic :: Scientific/Engineering :: Information Analysis', ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='Alexey Lavrenuke (load testing)',
       author_email='direvius@yandex-team.ru',
-      url='',
+      url='https://github.com/direvius/libgraphite',
       license='LGPLv3',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
